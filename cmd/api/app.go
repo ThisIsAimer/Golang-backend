071e79daa78bcff283ebe66034fe2834ad5d5a62
raunak <raunakbiswas05@gmail.com>
2025-07-20 11:24:29 +0530

    new api project for a school management system

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -1,141 +1,56 @@
 package main
 
 import (
-	"crypto/tls"
-	//"crypto/x509"
 	"fmt"
-	"log"
 	"net/http"
-	//"os"
-
-	"golang.org/x/net/http2"
 )
 
-func homeResp(w http.ResponseWriter, r *http.Request){
-	logRequestDetails(r)
+func homeRoute(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "string")
-	fmt.Fprintf(w,"our home page")
+	w.Write([]byte("This is home"))
+	fmt.Println("someone accessed: home")
 }
 
-func ordersResp(w http.ResponseWriter, r *http.Request){
-	logRequestDetails(r)
+func teachersRoute(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "string")
-	fmt.Fprintf(w,"handling incomming orders")
+	fmt.Fprintf(w,"This is teachers route")
+	fmt.Println("someone accessed: Teachers route")
 }
 
-func usersResp(w http.ResponseWriter, r *http.Request){
-	logRequestDetails(r)
+func studentsRoute(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "string")
-	fmt.Fprintf(w,"handling our users")
+	fmt.Fprintf(w,"This is students route")
+	fmt.Println("someone accessed: Students route")
 }
 
-//------------------------------------------------------------------
-// for reading client Ca
-// Now client must also have the pem certificate to access the page!
-
-// func loadClientCas() *x509.CertPool{
-// 	clientCas := x509.NewCertPool()
-// 	file, err := os.ReadFile(`certification\certificate.pem`)
-// 	if err != nil {
-// 		log.Fatalln("couldnt load client cert:", err)
-// 	}
-
-// 	clientCas.AppendCertsFromPEM(file)
-
-// 	return clientCas
-
-// }
-//------------------------------------------------------------------
+func execsRoute(w http.ResponseWriter, r *http.Request){
+	w.Header().Set("Content-Type", "string")
+	w.Write([]byte("This is executives route"))
+	fmt.Println("someone accessed: Execs route")
+}
 
 func main(){
 
-	http.HandleFunc("/", homeResp)
-	http.HandleFunc("/orders", ordersResp)
-	http.HandleFunc("/users", usersResp)
+	http.HandleFunc("/", homeRoute)
+	http.HandleFunc("/teachers", teachersRoute)
+	http.HandleFunc("/students", studentsRoute)
+	http.HandleFunc("/execs", execsRoute)
 
 
 	port := 3000
 
-	// for http2
-
-	cert := `certification\certificate.pem`
-	key := `certification\key.pem`
-
-	//configure tls
-	tlsConfig := &tls.Config{
-		MinVersion: tls.VersionTLS12,
-		//------------------------------------------------------------------
-		// client certification
-
-		// ClientAuth: tls.RequireAndVerifyClientCert,
-		// ClientCAs: loadClientCas(),
-		//------------------------------------------------------------------
-	}
-
-	//create a custom server
 	server := &http.Server{
 		Addr: fmt.Sprintf(":%d",port),
 		Handler: nil,
-		TLSConfig: tlsConfig,
 	}
 
-	// enable http2
-	// this configures the http server that we created to have https functionality
-	// its configuring the server
-	http2.ConfigureServer(server, &http2.Server{})
+	fmt.Println("server is running on port:", port)
 
 
-
-	fmt.Println("server is running on port: ", port)
-
-
-	err := server.ListenAndServeTLS(cert,key)
+	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println("error is:", err)
 		return
 	}
 
-
-	// http 1.1
-
-	// err := http.ListenAndServe(fmt.Sprintf(":%d",port),nil)
-	// if err != nil {
-	// 	fmt.Println("error is:", err)
-	// 	return
-	// }
-
 }
-
-
-func logRequestDetails(r *http.Request){
-	httpVersion := r.Proto
-	fmt.Println("-------------------------------------------------------------")
-	log.Println("recieved request with http version:", httpVersion)
-	if r.TLS != nil{
-		tlsVersion := tlsVersionDetails(r.TLS.Version)
-		log.Println("Tls version:", tlsVersion)
-	} else {
-		log.Println("no tls used")
-	}
-}
-
-func tlsVersionDetails(version uint16) string{
-	switch version{
-	case tls.VersionTLS10:
-		return "Tls 1.0"
-	case tls.VersionTLS11:
-		return "Tls 1.1"
-	case tls.VersionTLS12:
-		return "Tls 1.2"
-	case tls.VersionTLS13:
-		return "Tls 1.3"
-	default:
-		return "unknown TLS version"
-
-	}
-}
-
-
-// http: TLS handshake error from [::X]:XXXXX: EOF
-// TLS handshake error happens as there is smth wrong with the certification
-// EOF in networking means connection termination or protocal error
--- go.mod
+++ go.mod
@@ -1,7 +1,3 @@
 module simpleapi
 
 go 1.24.5
-
-require golang.org/x/net v0.41.0
-
-require golang.org/x/text v0.27.0 // indirect
