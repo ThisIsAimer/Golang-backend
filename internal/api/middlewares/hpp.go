7228994734af159949e199484d326633449e0c10
raunak <raunakbiswas05@gmail.com>
2025-07-28 07:41:44 +0530

    added the whitelisting function for HPP

--- internal/api/middlewares/hpp.go
+++ internal/api/middlewares/hpp.go
@@ -1,20 +1,57 @@
 package middlewares
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
 
-type HppOptions struct{
-	CheckQuary bool
-	CheckBody bool
+type HppOptions struct {
+	CheckQuary              bool
+	CheckBody               bool
 	CheckBodyForContentType string
-	WhiteList []string
-
+	WhiteList               []string
 }
 
-
-func Hpp(options HppOptions) func(http.Handler) http.Handler{
-	return func (next http.Handler) http.Handler{
+func Hpp(options HppOptions) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if options.CheckBody && r.Method == http.MethodPost && isCorrectContentType(r, options.CheckBodyForContentType) {
+				filterBodyParams(r, options.WhiteList)
+			}
 			next.ServeHTTP(w, r)
 		})
 	}
+}
+
+func isCorrectContentType(r *http.Request, contentType string) bool {
+	return strings.Contains(r.Header.Get("Content-Type"), contentType)
+}
+
+func filterBodyParams(r *http.Request, whitelist []string) {
+	err := r.ParseForm()
+	if err != nil {
+		fmt.Println("error is:", err)
+		return
+	}
+
+	for k, v := range r.Form {
+		if len(v) > 1 {
+			r.Form.Set(k, v[0]) //first value
+			// r.Form.Set(k,v[len(v)-1]) //last value
+		}
+
+		if !isWhiteListerd(k, whitelist) {
+			delete(r.Form, k)
+		}
+	}
+}
+
+func isWhiteListerd(param string, whitelist []string) bool {
+	for _, v := range whitelist {
+		if param == strings.ToLower(v) {
+			return true
+		}
+	}
+	return false
 }
