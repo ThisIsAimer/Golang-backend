640d403f773293ab531e72990302094ae7920b91
raunak <raunakbiswas05@gmail.com>
2025-08-01 10:41:38 +0530

    handling post method for teachers api

--- cmd/api/app.go
+++ cmd/api/app.go
@@ -15,11 +15,11 @@ import (
 
 // for now
 type Teacher struct {
-	ID         int
-	FirstName string
-	LastName   string
-	Class      string
-	Subject    string
+	ID        int    `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Class     string `json:"class"`
+	Subject   string `json:"subject"`
 }
 
 var (
@@ -30,70 +30,69 @@ var (
 
 func init() {
 	teachers[nextId] = Teacher{
-		ID:         nextId,
+		ID:        nextId,
 		FirstName: "Rudra",
-		LastName:   "Shivdev",
-		Class:      "6A",
-		Subject:    "math",
+		LastName:  "Shivdev",
+		Class:     "6A",
+		Subject:   "math",
 	}
 	nextId++
 
 	teachers[nextId] = Teacher{
-		ID:         nextId,
+		ID:        nextId,
 		FirstName: "Rudrina",
-		LastName:   "Shivdev",
-		Class:      "10B",
-		Subject:    "computer",
+		LastName:  "Shivdev",
+		Class:     "10B",
+		Subject:   "computer",
 	}
 
 	nextId++
 
 	teachers[nextId] = Teacher{
-		ID:         nextId,
+		ID:        nextId,
 		FirstName: "Tanjiro",
-		LastName:   "Kamado",
-		Class:      "all",
-		Subject:    "Dance",
+		LastName:  "Kamado",
+		Class:     "all",
+		Subject:   "Dance",
 	}
 
 	nextId++
 
 	teachers[nextId] = Teacher{
-		ID:         nextId,
+		ID:        nextId,
 		FirstName: "Zenitsu",
-		LastName:   "Agatsuma",
-		Class:      "8C",
-		Subject:    "Science",
+		LastName:  "Agatsuma",
+		Class:     "8C",
+		Subject:   "Science",
 	}
 
 	nextId++
 
 	teachers[nextId] = Teacher{
-		ID:         nextId,
+		ID:        nextId,
 		FirstName: "Inosuke",
-		LastName:   "Hashibira",
-		Class:      "5D",
-		Subject:    "Sports",
+		LastName:  "Hashibira",
+		Class:     "5D",
+		Subject:   "Sports",
 	}
 
 }
 
 func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
 
-	path := strings.TrimPrefix(r.URL.Path,"/teachers/")
-	idstr := strings.TrimSuffix(path,"/")
+	path := strings.TrimPrefix(r.URL.Path, "/teachers/")
+	idstr := strings.TrimSuffix(path, "/")
 
 	w.Header().Set("Content-Type", "application/json")
 
-
 	//handle quary parametre
-	if idstr == ""{
+	if idstr == "" {
 		firstName := r.URL.Query().Get("first_name")
 		lastName := r.URL.Query().Get("last_name")
 
 		teacherList := make([]Teacher, 0, len(teachers))
 		for _, teacher := range teachers {
-			if (firstName == "" || teacher.FirstName == firstName) && (lastName == "" || teacher.LastName == lastName){
+			if (firstName == "" || teacher.FirstName == firstName) && (lastName == "" || teacher.LastName == lastName) {
 				teacherList = append(teacherList, teacher)
 			}
 		}
@@ -108,23 +107,70 @@ func getTeachersHandler(w http.ResponseWriter, r *http.Request) {
 			Data:   teacherList,
 		}
 
-		json.NewEncoder(w).Encode(response)
-	} else{
+		err := json.NewEncoder(w).Encode(response)
+		if err != nil {
+			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+			return
+		}
+	} else {
 		//handle path parametre
 		id, err := strconv.Atoi(idstr)
 		if err != nil {
-			http.Error(w,"Invalid id",http.StatusBadRequest)
+			http.Error(w, "Invalid id", http.StatusBadRequest)
 			return
 		}
 
-		tearcher , exists :=  teachers[id]
-		if !exists{
-			http.Error(w,"Teacher not found",http.StatusNotFound)
+		tearcher, exists := teachers[id]
+		if !exists {
+			http.Error(w, "Teacher not found", http.StatusNotFound)
 			return
 		}
-		json.NewEncoder(w).Encode(tearcher)
+		err = json.NewEncoder(w).Encode(tearcher)
+		if err != nil {
+			http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+			return
+		}
 	}
 
+}
+
+func postTeachersHandler(w http.ResponseWriter, r *http.Request) {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	var newTeachers []Teacher
+	err := json.NewDecoder(r.Body).Decode(&newTeachers)
+	if err != nil {
+		http.Error(w, "invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	for i, teacher := range newTeachers {
+		nextId++
+		newTeachers[i].ID = nextId
+		
+		teacher.ID = nextId
+		teachers[nextId] = teacher
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+
+	response := struct {
+		Status string    `json:"status"`
+		Count  int       `json:"count"`
+		Data   []Teacher `json:"data"`
+	}{
+		Status: "Success",
+		Count:  len(newTeachers),
+		Data:   newTeachers,
+	}
+
+	err = json.NewEncoder(w).Encode(response)
+	if err != nil {
+		http.Error(w, "Failed to encode response", http.StatusInternalServerError)
+		return
+	}
 }
 
 // http methods are get, post, put, patch, delete
@@ -160,7 +206,7 @@ func teachersRoute(w http.ResponseWriter, r *http.Request) {
 	case http.MethodGet:
 		getTeachersHandler(w, r)
 	case http.MethodPost:
-		fmt.Fprintln(w, "accessed : Teachers. with: Post")
+		postTeachersHandler(w, r)
 	case http.MethodPut:
 		fmt.Fprintln(w, "accessed : Teachers. with: Put")
 	case http.MethodPatch:
