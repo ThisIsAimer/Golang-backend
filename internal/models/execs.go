faec6042b3f5d4a35b450cd5e8f2a4abf93335ad
raunak <raunakbiswas05@gmail.com>
2025-08-10 09:29:10 +0530

    Using db tags in golang

--- internal/models/execs.go
+++ internal/models/execs.go
@@ -1,7 +1,7 @@
 package models
 
-type Execs struct{
-	ID        int    `json:"id,omitempty"`
-	FirstName string `json:"first_name,omitempty"`
-	LastName  string `json:"last_name,omitempty"`
+type Execs struct {
+	ID        int    `json:"id,omitempty" db:"id,omitempty"`
+	FirstName string `json:"first_name,omitempty" db:"first_name,omitempty"`
+	LastName  string `json:"last_name,omitempty" db:"last_name,omitempty"`
 }
