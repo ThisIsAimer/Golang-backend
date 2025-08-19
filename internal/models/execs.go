b14d83465c5ae405c17b74137745cfad6a6bb2ce
raunak <raunakbiswas05@gmail.com>
2025-08-19 08:50:56 +0530

    execs struct model created

--- internal/models/execs.go
+++ internal/models/execs.go
@@ -1,7 +1,22 @@
 package models
 
+import "database/sql"
+
 type Execs struct {
 	ID        int    `json:"id,omitempty" db:"id,omitempty"`
 	FirstName string `json:"first_name,omitempty" db:"first_name,omitempty"`
 	LastName  string `json:"last_name,omitempty" db:"last_name,omitempty"`
+	Email     string `json:"email,omitempty" db:"email,omitempty"`
+	Role      string
+	UserName  string
+	Password  string
+	
+	// sql.NullStrings will automatically updated by the database so we dont need to populate it
+	// it can be null or a string
+	UserCreatedAt     sql.NullString
+	PasswordChangedAt sql.NullString
+	PassResetCode     sql.NullString
+	PassCodeExpires   sql.NullString
+
+	InactiveStatus    bool
 }
