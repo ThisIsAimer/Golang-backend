74b9e247689d966abe2737349269fce20bd57f64
raunak <raunakbiswas05@gmail.com>
2025-08-27 11:21:46 +0530

    using contexts for value transfer and finishing jwt authentication

--- internal/api/middlewares/jwt_middleware.go
+++ internal/api/middlewares/jwt_middleware.go
@@ -1,6 +1,7 @@
 package middlewares
 
 import (
+	"context"
 	"errors"
 	"fmt"
 	"log"
@@ -15,7 +16,7 @@ func JwtMiddleware(next http.Handler) http.Handler {
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		//fmt.Println(r.Cookies())
-		cookie, err := r.Cookie("Bearer")
+		token, err := r.Cookie("Bearer")
 		if err != nil {
 			http.Error(w, "unauthorised", http.StatusBadRequest)
 			return
@@ -23,10 +24,11 @@ func JwtMiddleware(next http.Handler) http.Handler {
 
 		jwtSecret := os.Getenv("JWT_SECRET")
 
-		parsedToken, err := jwt.Parse(cookie.Value, func(token *jwt.Token) (any, error) {
+		parsedToken, err := jwt.Parse(token.Value, func(token *jwt.Token) (any, error) {
 			// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
 			return []byte(jwtSecret), nil
 		}, jwt.WithValidMethods([]string{jwt.SigningMethodHS256.Alg()}))
+
 		if err != nil && r.URL.Path != "/execs/login" {
 			if errors.Is(err, jwt.ErrTokenExpired) {
 				myErr := utils.ErrorHandler(err, "token expired")
@@ -36,19 +38,32 @@ func JwtMiddleware(next http.Handler) http.Handler {
 			myErr := utils.ErrorHandler(err, "unauthorised access")
 			http.Error(w, myErr.Error(), http.StatusUnauthorized)
 			return
+		} else {
+			err = nil
 		}
 
 		if parsedToken.Valid {
 			log.Println("valid Jwt")
-		}
-
-		if claims, ok := parsedToken.Claims.(jwt.MapClaims); ok {
-			fmt.Println(claims["uid"], claims["exp"])
 		} else {
-			fmt.Println(err)
+			http.Error(w, "invalid token", http.StatusUnauthorized)
+			log.Println("invalid jwt:", token.Value)
 		}
 
-		next.ServeHTTP(w, r)
+		claims, ok := parsedToken.Claims.(jwt.MapClaims)
+
+		if ok {
+			fmt.Println(claims["uid"], claims["exp"], claims["role"])
+		} else {
+			http.Error(w, "invalid token", http.StatusUnauthorized)
+			return
+		}
+
+		ctx := context.WithValue(r.Context(), "role", claims["role"])
+		ctx = context.WithValue(ctx, "expiresAt", claims["exp"])
+		ctx = context.WithValue(ctx, "username", claims["user"])
+		ctx = context.WithValue(ctx, "userId", claims["uid"])
+
+		next.ServeHTTP(w, r.WithContext(ctx))
 	})
 
 }
